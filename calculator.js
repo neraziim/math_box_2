5f074ad552b43481223d75a07428df6a3f437e1e
neraziim <neraziim@gmail.com>
2025-12-01 11:37:31 +0900

    Update devide function

--- calculator.js
+++ calculator.js
@@ -8,3 +8,9 @@ const substract = (a, b) => a - b;
 const multiply =(a,b)=>{
   return a*b;
 };
+
+//type (x: number, y: number) -> number;
+const divide = (a, b) => {
+  if (b=== 0) return;
+  return a/b;
+};
